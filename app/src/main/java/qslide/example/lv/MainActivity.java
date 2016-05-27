0a14ed16e37d5c203545476d0608b939af21dcf2
gilibarak280 - Gili Barak <gilibarak280@gmail.com>
2016-05-27 17:31:24 +0300

    Gili - Add MainActivity

---
+++ app/src/main/java/qslide/example/lv/MainActivity.java
@@ -0,0 +1,47 @@
+package qslide.example.lv;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String[] mOptionsArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mOptionsArray = new String[]{"דיווחים", "מפגשים", "דג2", "צ'אט"};
+        ArrayAdapter myAdapter= new ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                mOptionsArray);
+
+        ListView myList=(ListView) findViewById(R.id.listView);
+        myList.setAdapter(myAdapter);
+        myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String option = mOptionsArray[position];
+                switch(option){
+                    case "דיווחים":
+                        break;
+                    case "מפגשים":
+                        Intent intent = new Intent(MainActivity.this, MeetingsActivity.class);
+                        startActivity(intent);
+                        break;
+                    case "דג2":
+                        break;
+                    case "צ'אט":
+                        break;
+                }
+            }
+        });
+    }
+}
